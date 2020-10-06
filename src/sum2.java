78e694f976c4983da79c967b4e8930ddef26890a
Stacychen <zoosin1mmm@gmail.com>
2020-10-06 13:21:22 +0000

    java avg sum2 sort

--- src/sum2.java
+++ src/sum2.java
@@ -10,33 +10,35 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
         
-public class sort {
+public class sum2 {
 
- public static class Map extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
+ public static class Map extends Mapper<LongWritable, Text, Text, LongWritable> {
+    long lsum=0;
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         StringTokenizer tokenizer = new StringTokenizer(line);
         while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken();
-            //auto sort key during shuffle,so set value in key 
-            context.write(new IntWritable(Integer.parseInt(token)),new IntWritable(1));
+            //context.write(new Text("test"), new IntWritable(Integer.parseInt(tokenizer.nextToken())));
+            //context.write(new Text("test"), new IntWritable(Integer.parseInt(token)));
+            lsum+=Integer.parseInt(token);
         }
     }
+    //run this function after mapper finish
+    public void cleanup(Context context) throws IOException, InterruptedException {
+       context.write(new Text("test"), new LongWritable(lsum));
+   }
  } 
         
- public static class Reduce extends Reducer<IntWritable, IntWritable, Text, IntWritable> {
+ public static class Reduce extends Reducer<Text, LongWritable, Text, LongWritable> {
 
-    public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) 
+    public void reduce(Text key, Iterable<LongWritable> values, Context context) 
       throws IOException, InterruptedException {
-        //long sum = 0;
-        //int count=0;
-        for (IntWritable val : values) {
-           // sum += val.get();
-           // count++;
-           context.write(new Text(""), key);
+        long sum = 0;
+        for (LongWritable val : values) {
+            sum += val.get();
         }
-        //sum = sum/count;
-        //context.write(key, new LongWritable(sum));
+        context.write(key, new LongWritable(sum));
     }
  }
         
@@ -46,13 +48,11 @@ public class sort {
         Job job = new Job(conf, "sum");
     
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    job.setMapOutputKeyClass(IntWritable.class);
-    //job.setMapOutputValueClass(Text.class);
+    job.setOutputValueClass(LongWritable.class);
         
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
-    job.setJarByClass(sort.class);
+    job.setJarByClass(sum2.class);
         
     job.setInputFormatClass(TextInputFormat.class);
     job.setOutputFormatClass(TextOutputFormat.class);
